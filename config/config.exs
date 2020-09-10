44c4bb79f2606b5280c72bf78234c7a8805b9bb8
Arnaud Morisset <arnaudmorisset@protonmail.com>
2020-09-10 13:13:03 +0200

    Add custom dev config

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,5 @@
 import Config
 
-config :shorty, Shorty.Repo,
-  database: "shorty_dev",
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost"
-
 config :shorty, ecto_repos: [Shorty.Repo]
 
 config :shorty, cowboy_port: 8080
---
+++ config/dev.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+config :shorty, Shorty.Repo,
+  database: "shorty_dev",
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost"
