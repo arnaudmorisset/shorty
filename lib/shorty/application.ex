f0747ee3c45ce7c584664a2bfcb4d6a5a283fc73
Arnaud Morisset <arnaudmorisset@protonmail.com>
2020-09-21 18:34:25 +0200

    Make Gigalixir's port the default one

--- lib/shorty/application.ex
+++ lib/shorty/application.ex
@@ -6,10 +6,7 @@ defmodule Shorty.Application do
   def start(_type, _args) do
     children = [
       Shorty.Repo,
-      {Plug.Cowboy,
-       scheme: :http,
-       plug: Shorty.Router,
-       options: [port: Application.get_env(:shorty, :cowboy_port, 8080)]}
+      {Plug.Cowboy, scheme: :http, plug: Shorty.Router, options: [port: 4000]}
     ]
 
     opts = [strategy: :one_for_one, name: Shorty.Supervisor]
